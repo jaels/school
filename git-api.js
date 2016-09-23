a60c533e7b1a3c6027e1329a62b8c5b85a7e69f0
Yael Stein <yaelst@gmail.com>
2016-09-23 18:02:16 +0200

    GitHub API

---
+++ git-api.js
@@ -0,0 +1,121 @@
+(function () {
+
+
+    var templates = document.querySelectorAll('script[type="text/handlebars"]');
+
+    Handlebars.templates = Handlebars.templates || {};
+
+
+    Array.prototype.slice.call(templates).forEach(function(script) {
+        Handlebars.templates[script.id] = Handlebars.compile(script.innerHTML);
+    });
+
+
+    $('#go-button').on('click',search);
+
+
+    function search() {
+        var username = $("#typing-field").val();
+        $.ajax({
+            url: 'https://api.github.com/users/'+ username + "/repos",
+            method: 'GET',
+            data: {
+                type: 'all'
+            },
+            headers: {
+                Authorization: 'Basic ' + btoa('jaels:sarusi1979')
+            },
+            success: function(data) {
+
+
+
+                var obj;
+                var newArr = [];
+                var commitsArr = [];
+                var commitsObj;
+
+                for (var i=0;i<data.length;i++){
+                    obj={};
+                    obj.pic = data[i].owner.avatar_url;
+                    obj.repoName = data[i].name;
+                    obj.fullRepoName = data[i].full_name;
+                    obj.commits = data[i].commits_url;
+                    obj.owner = data[i].owner.login;
+                    obj.id = i;
+
+                    newArr[i] = obj;
+                }
+                var location;
+                $("#results").append(Handlebars.templates.resultsScript({newArr: newArr}));
+
+                $(".fullRepoName").on("click", function(e) {
+                    $(".results").empty();
+
+                    var chosenRepo = e.target;
+
+                    var pressedRepo = e.target.innerHTML;
+                    for (var i=0;i<newArr.length;i++) {
+                        if(newArr[i].fullRepoName===pressedRepo) {
+                            location=i;
+                        }
+                    }
+
+                    var number;
+
+
+                    $.ajax({
+                        url: 'https://api.github.com/repos/' + newArr[location].owner + "/" + newArr[location].repoName + '/commits',
+                        method: 'GET',
+                        data: {
+                            type: 'all'
+                        },
+                        headers: {
+                            Authorization: 'Basic ' + btoa('jaels:sarusi1979')
+                        },
+                        success: function(data1) {
+                            if (data1.length>10) {number=10;}
+                            else {number=data1.length;}
+                            if (commitsArr.length>0){
+                                commitsArr=[];
+                            }
+                            for(var i=0;i<number;i++) {
+                                commitsObj={};
+                                commitsObj.message = data1[i].commit.message;
+                                commitsArr.push(commitsObj);
+                            }
+
+                            $("#"+location).append(Handlebars.templates.commitsScript({commitsArr: commitsArr}));
+
+
+
+                        }
+
+                    });
+
+                });
+
+
+            }
+
+        });
+
+
+        $('#go-button').on('click',function() {
+            $("#results").empty();
+            console.log("hey");
+            search();
+
+        });
+
+
+
+    }
+
+
+
+
+
+
+
+
+})();
--- wk2/tuesday_dom/exercise1.js
+++ wk2/tuesday_dom/exercise1.js
@@ -1,11 +1,33 @@
 
 
 (function() {
-    var box = document.getElementsByClassName("box")[0];
-    box.addEventListener("mousemove", function() {
+
+<!--the original version with just mousemove is in github-->
+    function move() {
         var x = event.clientX;
         var y = event.clientY;
-        box.style.left = x - 50 + "px";
-        box.style.top = y - 50 + "px";
-    });
+        box.style.left = x + "px";
+        box.style.top = y + "px";
+}
+
+    var box = document.getElementsByClassName("box")[0];
+
+    box.addEventListener("mousedown", function() {
+    box.addEventListener("mousemove", move);
+
+
+
+});
+
+
+box.addEventListener("mouseup", function() {
+
+    box.removeEventListener("mousemove", move);
+
+
+});
+
+
+
+
 })();
